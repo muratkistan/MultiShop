ca638ea6968304a54db1dc105a00f315e3dc16c3
muratkistan <muratkistan@gmail.com>
2024-07-11 15:12:22 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]//Benzersiz bir id oluşturur
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
