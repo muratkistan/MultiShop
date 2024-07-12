ca7b7e21b7278fc4c41e0cc422a759d974dfb614
muratkistan <muratkistan@gmail.com>
2024-07-12 11:53:26 +0300

    Discount servisi eklendi.

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using Npgsql;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=discountDb;Username=postgres;Password=0,369*/;Include Error Detail=true");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Migrations/20240712075616_mig1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MultiShop.Discount.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    CouponId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    Rate = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    ValidDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.CouponId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
