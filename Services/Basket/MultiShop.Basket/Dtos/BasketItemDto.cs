3bb4c4c73a32287d2fd8e1ec1fcdeaf80816c1b5
muratkistan <muratkistan@gmail.com>
2024-07-17 17:45:32 +0300

    Basket servisi olusturuldu ve redise baglanti saglandi

---
+++ Services/Basket/MultiShop.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public string MyProperty { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
